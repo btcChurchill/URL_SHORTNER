15a3f2231adf8f79b0dd78b0a809f973345ff6a4
Nwakerendu Churchill <Churchillnwa@gmail.com>
2022-05-09 16:32:00 +0100

    url-shortner update

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+const uri = process.env.MONGODB_URI || 'mongodb+srv://admin:churchill123@cluster0.nbivu.mongodb.net/shortUrl?retryWrites=true&w=majority';
+
+mongoose.connect(uri, {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to Database')
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+}); // listen on port 3000
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected')
+});// if mongoose is connected
+  
+
